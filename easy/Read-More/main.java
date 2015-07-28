6d04623f56bb3e2b72dd7197784be9b26aee38d7
diegolmendonca <diego37@gmail.com>
2015-07-27 23:58:03 -0300

    Create main.java

---
+++ easy/Read-More/main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		File file = new File(args[0]);
+		BufferedReader buffer = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = buffer.readLine()) != null) {
+			line = line.trim();
+			if (line.length() <= 55) {
+				System.out.println(line);
+			} else {
+				System.out.println(buildTrim(line));
+			}
+		}
+	}
+
+	private static String buildTrim(String line) {
+		String[] splitted = line.substring(0, 40).split(" ");
+		StringBuilder builder = new StringBuilder();
+
+		if (splitted[0].length() >= 40) {
+			return splitted[0].substring(0, 40) + "... <Read More>";
+		}
+
+		for (String string : splitted) {
+			if (builder.length() + string.length() < 40) {
+				builder.append(string + " ");
+			}
+		}
+
+		return builder.toString().trim() + "... <Read More>";
+	}
+}
